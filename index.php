ebf24a80a649a20933c63db825108445bf69bb5e
Ashilpa Darrel Bumika <ashilpa.darrel@gmail.com>
2024-06-06 01:24:07 +0700

    update landing page

--- index.php
+++ index.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <title>SPK - SAW</title>
     <link rel="stylesheet" href="./src/output.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="">
-    <div class="w-svw h-svh flex justify-center items-center relative">
-        <img src="./src/images/landing-bg.jpg" alt="" class="w-full h-full absolute -z-50">
-        <div class="w-[800px] flex flex-col gap-10 items-center">
-            <div class="flex flex-col gap-5 items-center">
-                <p class="font-poppins text-5xl text-white text-center">Sistem Pendukung Keputusan Metode SAW</p>
-                <p class="font-poppins text-l text-white">Berbasis Website</p>
-            </div>
-            <a class="w-[154px] h-[40px] flex justify-center items-center hover:cursor-pointer bg-primary-main rounded-full font-poppins font-semibold text-l text-white transition-all ease-in-out hover:w-[184px]" href="./sequence.php">Home</a>
+    <div class="w-svw h-svh flex justify-center items-center">
+        <div class="w-[1124px] h-[377px] flex gap-[100px]">
+            <div class="w-[800px] flex flex-col gap-10 justify-center">
+                <div class="flex flex-col gap-5 items-start">
+                    <p class="font-poppins font-extrabold text-5xl text-black text-left">Sistem Pendukung Keputusan Metode SAW</p>
+                    <p class="font-poppins text-l text-primary-main">Berbasis Website</p>
+                </div>
+                <a class="w-[154px] h-[40px] flex justify-center items-center hover:cursor-pointer bg-primary-main rounded-full font-poppins font-semibold text-l text-white transition-all ease-in-out hover:w-[184px]" href="./sequence.php">Home</a>
+            </div> 
+            <img src="./src/images/landing-bg.jpg" alt="" class="w-[610px] h-[377px] object-fill">
         </div>
     </div>
 </body>
